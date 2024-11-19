d838c8c3448c463a57175e09bcf045f14a90021b
Thalisson Moura <thalissonmoura138@gmail.com>
2024-11-19 15:09:11 -0300

    :recycle:refactor:issue #01 adiciona componente header

---
+++ src/ui/button/header/index.tsx
@@ -0,0 +1,16 @@
+import "./style.css";
+
+const Header = () => {
+    return (
+        <header>
+            <nav className="nav-itens">
+                <a href="">Alunos</a>
+                <a href="">Cursos</a>
+                <a href="">Usuários</a>
+                <a href="">Permissões</a>
+            </nav>
+        </header>
+    )
+};
+
+export {Header}
