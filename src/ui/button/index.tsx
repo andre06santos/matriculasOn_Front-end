46fc96f1a5c5cbfcdb31fd5ed00256689bbf93e6
GiseleBCSantos <catce.2024111tads0013@aluno.ifpi.edu.br>
2024-11-21 08:39:31 -0300

    #1 :recycle: refactor: Altera component Button

--- src/ui/button/index.tsx
+++ src/ui/button/index.tsx
@@ -1,41 +1,47 @@
-import { useState } from "react"
-import "./styles.css"
+import { useState } from "react";
+import "./styles.css";
+import { Link } from "react-router-dom";
 
-const Button = ({text, onClick, type, select=false, selectOptions}:any) => {
-    const [isSelected, setIsSelected] = useState(false)
+const Button = ({ label, onClick, type = "cancel", selectOptions }: any) => {
+  const [isExpanded, setIsExpanded] = useState(false);
 
-    const buttonClasses:any = {
-        "success": "btn-success",
-        "cancel": "btn-cancel",
-        "danger": "btn-danger"
-    }
-    
+  const buttonClasses: any = {
+    success: "btn-success",
+    cancel: "btn-cancel",
+    danger: "btn-danger",
+  };
 
-    const buttonClass = `button ${buttonClasses[type]}`
+  const buttonClass = `button ${buttonClasses[type]} ${
+    selectOptions && "select"
+  }`;
 
-    const handleChangeSelect = () => {
-        setIsSelected(prevState => !prevState)
-    }
+  const handleChangeExpanded = () => {
+    setIsExpanded((prevState) => !prevState);
+  };
 
   return (
     <div className="button-component">
-        <button onClick={select ? handleChangeSelect : onClick} className={buttonClass}>
-            {text} 
-            {select && <i className={`fa-solid fa-caret-up ${isSelected && "selected"}`}></i>}
-        </button>
+      <button
+        onClick={selectOptions ? handleChangeExpanded : onClick}
+        className={buttonClass}
+      >
+        {label}
+        {selectOptions && (
+          <i className={`fa-solid fa-caret-up ${isExpanded && "selected"}`}></i>
+        )}
+      </button>
 
-        {select && isSelected &&
-        
+      {isExpanded && (
         <ul className="button-select">
-            {
-                selectOptions.map((option:any) => (
-                    <li>{option}</li>
-                ))
-            }
+          {selectOptions.map((option: any) => (
+            <Link to={option.path} key={option.id}>
+              <li key={option.id}>{option.label}</li>
+            </Link>
+          ))}
         </ul>
-        }
+      )}
     </div>
-  )
-}
+  );
+};
 
-export {Button}
+export { Button };
