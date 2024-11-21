9bad4af156e1e7d7a54fda6d9a0a4c759691afd4
GiseleBCSantos <catce.2024111tads0013@aluno.ifpi.edu.br>
2024-11-21 08:40:48 -0300

    #1 :recycle: refactor: Altera nome de props e remove comentarios

--- src/ui/input/index.tsx
+++ src/ui/input/index.tsx
@@ -1,51 +1,44 @@
-import Select from "react-select"
-import "./styles.css"
-import { Fragment } from "react/jsx-runtime"
+import Select from "react-select";
+import "./styles.css";
+import { Fragment } from "react/jsx-runtime";
 
-const Input = ({label, type, select=false, selectOptions, ...rest}:any) => {
-    const inputClasses:any = {
-        text: 'input-text',
-        password: "input-text",
-        reset: "input-button reset",
-        submit: 'input-button submit'
-    }
+const Input = ({
+  label,
+  type,
+  selectOptions,
+  ...rest
+}: any) => {
+  const inputClasses: any = {
+    text: "input-text",
+    password: "input-text",
+    reset: "input-button reset",
+    submit: "input-button submit",
+  };
 
-    const inputClass = `input ${inputClasses[type]}`
+  const inputClass = `input ${inputClasses[type]}`;
 
   return (
     <div className="input-component">
-        {label && <label>{label}</label>}
+      {label && <label>{label}</label>}
 
-        {
-            select ? 
-            <Fragment>
-
-            <Select 
-                defaultValue={selectOptions[0]}
-                isClearable
-                isSearchable
-                options={selectOptions}
-                getOptionValue={(option:any) => `${option['id']}`}
-                getOptionLabel={(option:any) => `${option["text"]}`}
-                className="input-select"
-                />
-            </Fragment>
-            // <select>
-            //     {
-            //         selectOptions.map((option:any) => (
-            //             <option key={option.id} {...rest}>{option.text}</option>
-
-            //         ))
-            //     }
-            // </select>
-
-            :
-
-            <input type={type} {...rest} className={inputClass}/>
-
-        }
+      {selectOptions ? (
+        <Fragment>
+          <Select
+            defaultValue={selectOptions[0]}
+            isClearable
+            isSearchable
+            options={selectOptions}
+            getOptionValue={(option: any) => `${option["id"]}`}
+            getOptionLabel={(option: any) => `${option["text"]}`}
+            className="input-select"
+          />
+        </Fragment>
+      ) : (
+        
+        <input type={type} {...rest} className={inputClass} />
+      )}
     </div>
-  )
-}
+  );
+};
 
-export {Input}
+export { Input };
