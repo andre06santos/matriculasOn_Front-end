ea3e24c8adafcfd637ed169085c072ddf89e9284
GiseleBCSantos <catce.2024111tads0013@aluno.ifpi.edu.br>
2024-11-22 08:25:56 -0300

    #28 :sparkles: feat: Adiciona Links ao Header

--- src/ui/header/index.tsx
+++ src/ui/header/index.tsx
@@ -1,16 +1,27 @@
+import { Link } from "react-router-dom";
 import "./styles.css";
 
 const Header = () => {
-    return (
-        <header>
-            <nav className="nav-itens">
-                <a href="">Alunos</a>
-                <a href="">Cursos</a>
-                <a href="">Usuários</a>
-                <a href="">Permissões</a>
-            </nav>
-        </header>
-    )
+  return (
+    <header>
+      <nav className="nav-itens">
+        <ul className="nav-links">
+          <Link to="/alunos">
+            <li>Alunos</li>
+          </Link>
+          <Link to="/cursos">
+            <li>Cursos</li>
+          </Link>
+          <Link to="/usuarios">
+            <li>Usuários</li>
+          </Link>
+          <Link to="/permissoes">
+            <li>Permissões</li>
+          </Link>
+        </ul>
+      </nav>
+    </header>
+  );
 };
 
-export { Header }
+export { Header };
