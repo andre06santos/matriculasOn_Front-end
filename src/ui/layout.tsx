f96a40ba62d780ad83a49cf4662144471fc8b00e
andre06santos <andrecontagitlab@gmail.com>
2024-11-22 15:49:10 -0300

    feat: issue #18 implementa a página de login

--- src/ui/layout.tsx
+++ src/ui/layout.tsx
@@ -1,14 +1,16 @@
 import { Outlet } from "react-router-dom";
 import { Header } from "./header";
+import { LoginPage } from "../pages/home";
 
 const Layout = () => {
   return (
-    <>
-      <Header />
-      <div className="main-route">
-        <Outlet />
-      </div>
-    </>
+    // <>
+    //   <Header />
+    //   <div className="main-route">
+    //     {/* <Outlet /> */}
+    //   </div>
+    // </>
+    <LoginPage />
   );
 };
 
