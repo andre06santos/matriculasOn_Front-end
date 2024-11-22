347054cd0110400c6a5efe6cae88984bea9664bb
GiseleBCSantos <catce.2024111tads0013@aluno.ifpi.edu.br>
2024-11-22 09:43:37 -0300

    #28 :recycle: refactor: Mudança de rotas para aninhamento

--- src/App.tsx
+++ src/App.tsx
@@ -1,37 +1,44 @@
 import { BrowserRouter, Route, Routes } from "react-router-dom";
 import "./App.css";
 import { Layout } from "./ui/layout";
-import { ListPermissions } from "./pages/permissao/list";
-import { CreatePermission } from "./pages/permissao/cadastro";
-import { EditPermission } from "./pages/permissao/edicao";
+import { ListPermissions } from "./pages/permissions/list";
+import { CreatePermission } from "./pages/permissions/create";
+import { EditPermission } from "./pages/permissions/edit";
 import { CourseRegistration } from "./pages/cursos/cadastro";
 import { EditCourse } from "./pages/cursos/edicao";
 
 const App = () => {
   return (
-    <>
-      <BrowserRouter>
-        <Routes>
-          <Route path="/" element={<Layout />}>
-            <Route path="/permissoes" element={<ListPermissions />} />
-            <Route path="/nova-permissao" element={<CreatePermission />} />
-            <Route path="/editar-permissao" element={<EditPermission />} />
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<Layout />}>
+          <Route path="/permissoes" element={<ListPermissions />} />
+          <Route
+            path="/permissoes/nova-permissao"
+            element={<CreatePermission />}
+          />
+          <Route
+            path="/permissoes/editar-permissao"
+            element={<EditPermission />}
+          />
 
-            <Route path="/cursos" element={<ListPermissions />} />
-            <Route path="/novo-curso" element={<CourseRegistration />} />
-            <Route path="/editar-curso" element={<EditCourse />} />
+          <Route path="/cursos" element={<ListPermissions />} />
+          <Route path="/cursos/novo-curso" element={<CourseRegistration />} />
+          <Route path="/cursos/editar-curso" element={<EditCourse />} />
 
-            <Route path="/alunos" element={<ListPermissions />} />
-            <Route path="/novo-aluno" element={<CourseRegistration />} />
-            <Route path="/editar-aluno" element={<EditCourse />} />
+          <Route path="/alunos" element={<ListPermissions />} />
+          <Route path="/alunos/novo-aluno" element={<CourseRegistration />} />
+          <Route path="/alunos/editar-aluno" element={<EditCourse />} />
 
-            <Route path="/usuarios" element={<ListPermissions />} />
-            <Route path="/novo-usuario" element={<CourseRegistration />} />
-            <Route path="/editar-usuario" element={<EditCourse />} />
-          </Route>
-        </Routes>
-      </BrowserRouter>
-    </>
+          <Route path="/usuarios" element={<ListPermissions />} />
+          <Route
+            path="/usuarios/novo-usuario"
+            element={<CourseRegistration />}
+          />
+          <Route path="/usuarios/editar-usuario" element={<EditCourse />} />
+        </Route>
+      </Routes>
+    </BrowserRouter>
   );
 };
 
---
+++ src/pages/permissions/list/index.tsx
@@ -0,0 +1,6 @@
+        <Link to="/permissoes/nova-permissao">
+          <Button type="success" label="Adicionar" />
+        </Link>
+                <Link to="/permissoes/editar-permissao">
+                  <i className="fa-solid fa-pen-to-square"></i>
+                </Link>
