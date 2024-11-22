331a049735c497e177e9681c4c642f7c1adbfa80
andre06santos <andrecontagitlab@gmail.com>
2024-11-22 16:59:31 -0300

    fix: desfaz implementações de teste de código

--- src/ui/layout.tsx
+++ src/ui/layout.tsx
@@ -1,16 +1,14 @@
 import { Outlet } from "react-router-dom";
 import { Header } from "./header";
-import { LoginPage } from "../pages/home";
 
 const Layout = () => {
   return (
-    // <>
-    //   <Header />
-    //   <div className="main-route">
-    //     {/* <Outlet /> */}
-    //   </div>
-    // </>
-    <LoginPage />
+    <>
+      <Header />
+      <div className="main-route">
+        <Outlet />
+      </div>
+    </>
   );
 };
 
