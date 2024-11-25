f3c8705c38930930265d7d96378d7a7de030ac49
Thalisson Moura <thalissonmoura138@gmail.com>
2024-11-25 10:14:22 -0300

    refactor: renomeia classes para melhor semântica

--- src/pages/administradores/cadastro/index.tsx
+++ src/pages/administradores/cadastro/index.tsx
@@ -7,21 +7,21 @@ const AdministratorRegistration = () => {
     <div className="flex-column-gap20">
       <h1>Cadastrar administrador</h1>
       <form className="form">
-        <div className="container-forms ">
+        <div className="input-group">
           <Input label="CPF" type="text" />
           <Input label="Cargo" type="text" />
           <Input label="Nome" type="text" />
         </div>
-        <div className="container-forms">
+        <div className="input-group">
           <Input label="Username" type="text" />
           <Input label="Email" type="text" />
           <Input label="Departamento" type="text" />
         </div>
-        <div className="container-forms ">
-          <Input label="Senha" type="text" />
-          <Input label="Confirmar senha" type="text" />
+        <div className="input-group ">
+          <Input label="Senha" type="password" />
+          <Input label="Confirmar senha" type="password" />
         </div>
-        <div className="container-buttons flex-column-gap20">
+        <div className="form-actions flex-column-gap20">
           <Input type="reset" variant="bgNeutral" value="Limpar" />
           <Button type="cancel" label="Cancelar" />
           <Input type="submit" variant="bgSuccess" value="Cadastrar" />
