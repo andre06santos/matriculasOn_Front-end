d52cee6e41ef879c9dcbdb66656f86c316902ff5
Thalisson Moura <thalissonmoura138@gmail.com>
2024-11-25 10:17:46 -0300

    refactor: renomeia classes para melhor semântica

--- src/pages/alunos/cadastro/index.tsx
+++ src/pages/alunos/cadastro/index.tsx
@@ -6,21 +6,21 @@ const RegisterStudent = () => {
     <div className="flex-column-gap20">
       <h1>Cadastrar aluno</h1>
       <form className="form">
-        <div className="container-forms ">
+        <div className="input-group">
           <Input label="CPF" type="text" />
           <Input label="Matrícula" type="text" />
           <Input label="Nome" type="text" />
         </div>
-        <div className="container-forms">
+        <div className="input-group">
           <Input label="Username" type="text" />
           <Input label="Email" type="text" />
           <Input label="Curso" type="text" />
         </div>
-        <div className="container-forms ">
-          <Input label="Senha" type="text" />
-          <Input label="Confirmar senha" type="text" />
+        <div className="input-group">
+          <Input label="Senha" type="password" />
+          <Input label="Confirmar senha" type="password" />
         </div>
-        <div className="container-buttons flex-column-gap20">
+        <div className="form-actions flex-column-gap20">
           <Input type="reset" variant="bgNeutral" value="Limpar" />
           <Button type="cancel" label="Cancelar" />
           <Input type="submit" variant="bgSuccess" value="Cadastrar" />
