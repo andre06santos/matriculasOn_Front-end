7485bbb286125e2b02b2f6738413b84e384e8fc1
Thalisson Moura <thalissonmoura138@gmail.com>
2024-11-27 09:54:19 -0300

    :recycle:refactor:adiciona classe e id no cadastro pra adicona um estilo especifico

--- src/pages/cursos/cadastro/index.tsx
+++ src/pages/cursos/cadastro/index.tsx
@@ -7,16 +7,16 @@ const CourseRegistration = () => {
   return (
     <div className="flex-column-gap20">
       <h1>Cadastrar curso</h1>
-      <form className="form flex-column-gap20">
+      <form className="form-registration flex-column-gap20">
         <div className="flex-column-gap20">
           <Input label="Nome" type="text" />
         </div>
-        <div className="container-buttons flex-column-gap20">
+        <div className="form-actions-registration flex-column-gap20" id="removePadding">
           <Input type="reset" variant="bgNeutral" value="Limpar" />
           <Link to="/cursos">
             <Button type="cancel" label="Cancelar" />
           </Link>
-          <Input type="submit" variant="bgSuccess" value="Cadastrar" />
+          <Input type="submit" variant="bgSuccess" value="Cancelar" />
         </div>
       </form>
     </div>
