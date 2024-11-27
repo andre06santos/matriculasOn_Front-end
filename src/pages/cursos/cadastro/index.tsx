9845f23a7463f563384a7614c8601fc297194b82
GiseleBCSantos <catce.2024111tads0013@aluno.ifpi.edu.br>
2024-11-27 14:14:26 -0300

    #71 :sparkles: feat: Adiciona validação da criação de cursos

--- src/pages/cursos/cadastro/index.tsx
+++ src/pages/cursos/cadastro/index.tsx
@@ -1,15 +1,50 @@
 import "./styles.css";
 import { Input } from "../../../ui/input";
 import { Button } from "../../../ui/button";
-import { Link } from "react-router-dom";
+import { Link, useNavigate } from "react-router-dom";
+import { useState } from "react";
+import { toast } from "react-toastify";
 
 const CourseRegistration = () => {
+  const navigate = useNavigate();
+  const [nome, setNome] = useState("");
+
+  const verificarApenasLetras = (nome: any) => {
+    const apenasLetras = /^[a-zA-ZÀ-ÿ]+$/.test(nome);
+    return apenasLetras;
+  };
+
+  const handleSubmit = (e: any) => {
+    e.preventDefault();
+    const novoErro: any = {};
+
+    novoErro["nomeErro"] = verificarApenasLetras(nome)
+      ? ""
+      : "Nome so pode ter letras";
+
+    const listaErros = Object.values(novoErro).filter((error) => error !== "");
+
+    if (listaErros.length > 0) {
+      console.log(listaErros[0]);
+
+      return;
+    } else {
+      navigate("/cursos");
+    }
+  };
+
   return (
     <div className="flex-column-gap20">
       <h1>Cadastrar curso</h1>
-      <form className="form flex-column-gap20">
+      <form className="form flex-column-gap20" onSubmit={handleSubmit}>
         <div className="flex-column-gap20">
-          <Input label="Nome" type="text" />
+          <Input
+            label="Nome"
+            type="text"
+            id="nome"
+            required
+            onChange={(e: any) => setNome(e.target.value)}
+          />
         </div>
         <div className="container-buttons flex-column-gap20">
           <Input type="reset" variant="bgNeutral" value="Limpar" />
