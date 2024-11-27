175f733e2226b7ac38e7846a9e6fe2f7c33f44d1
Thalisson Moura <thalissonmoura138@gmail.com>
2024-11-27 10:34:35 -0300

    :recycle:refactor: remove id da pagina de cadastro

--- src/pages/cursos/cadastro/index.tsx
+++ src/pages/cursos/cadastro/index.tsx
@@ -11,7 +11,7 @@ const CourseRegistration = () => {
         <div className="flex-column-gap20">
           <Input label="Nome" type="text" />
         </div>
-        <div className="form-actions-registration flex-column-gap20" id="removePadding">
+        <div className="form-actions-registration flex-column-gap20">
           <Input type="reset" variant="bgNeutral" value="Limpar" />
           <Link to="/cursos">
             <Button type="cancel" label="Cancelar" />
