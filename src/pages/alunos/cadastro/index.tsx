74f433fbfa0fbae7433c5609fbcb3bdeb426d014
GiseleBCSantos <catce.2024111tads0013@aluno.ifpi.edu.br>
2024-11-28 17:23:41 -0300

    #71 :sparkles: feat: Adiciona validação ao formulario

--- src/pages/alunos/cadastro/index.tsx
+++ src/pages/alunos/cadastro/index.tsx
@@ -1,8 +1,31 @@
 import "./styles.css";
 import { Input } from "../../../ui/input";
 import { Button } from "../../../ui/button";
-import { Link } from "react-router-dom";
+import { Link, useNavigate } from "react-router-dom";
+import { useState } from "react";
+import {
+  handleChangeConfSenha,
+  handleChangeCpf,
+  handleChangeEmail,
+  handleChangeMatricula,
+  handleChangeNome,
+  handleChangeSenha,
+  handleChangeUsername,
+  verificaSenhasIguais,
+} from "../../../modules/alunosFormValidation";
+
 const RegisterStudent = () => {
+  const [cpf, setCpf] = useState("");
+  const [matricula, setMatricula] = useState("");
+  const [nome, setNome] = useState("");
+  const [username, setUsername] = useState("");
+  const [email, setEmail] = useState("");
+  const [curso, setCurso] = useState("");
+  const [senha, setSenha] = useState("");
+  const [confSenha, setConfSenha] = useState("");
+  const [errorMessages, setErrorMessages] = useState({});
+  const navigate = useNavigate();
+
   const cursoOptions = [
     { id: 1, text: "Análise e Desenvolvimento de Sistemas" },
     { id: 2, text: "Engenharia de Software" },
@@ -10,23 +33,104 @@ const RegisterStudent = () => {
     { id: 4, text: "Tecnologia da Informação" },
   ];
 
+  const handleSubmit = (e: any) => {
+    e.preventDefault();
+
+    const listaErros = Object.values(errorMessages).filter(
+      (error) => error !== ""
+    );
+
+    if (listaErros.length > 0) {
+      console.log(listaErros[0]);
+
+      return;
+    } else {
+      navigate("/usuarios");
+    }
+  };
+
   return (
     <div className="flex-column-gap20">
       <h1>Cadastrar aluno</h1>
-      <form className="form">
+      <form className="form" onSubmit={handleSubmit}>
         <div className="input-group">
-          <Input label="CPF" type="text" />
-          <Input label="Matrícula" type="text" />
-          <Input label="Nome" type="text" />
+          <Input
+            label="CPF"
+            type="text"
+            value={cpf}
+            required
+            onChange={(e: any) => handleChangeCpf(e, setErrorMessages, setCpf)}
+          />
+          <Input
+            label="Matrícula"
+            type="text"
+            required
+            value={matricula}
+            onChange={(e: any) =>
+              handleChangeMatricula(e, setErrorMessages, setMatricula)
+            }
+          />
+          <Input
+            label="Nome"
+            type="text"
+            required
+            value={nome}
+            onChange={(e: any) =>
+              handleChangeNome(e, setErrorMessages, setNome)
+            }
+          />
         </div>
         <div className="input-group">
-          <Input label="Username" type="text" />
-          <Input label="Email" type="text" />
-          <Input label="Curso" selectOptions={cursoOptions} />
+          <Input
+            label="Username"
+            type="text"
+            value={username}
+            required
+            onChange={(e: any) =>
+              handleChangeUsername(e, setErrorMessages, setUsername)
+            }
+          />
+          <Input
+            label="Email"
+            type="text"
+            value={email}
+            required
+            onChange={(e: any) =>
+              handleChangeEmail(e, setErrorMessages, setEmail)
+            }
+          />
+          <Input
+            label="Curso"
+            selectOptions={cursoOptions}
+            value={curso}
+            onChange={(e: any) => setCurso(e.text)}
+          />
         </div>
         <div className="input-group">
-          <Input label="Senha" type="password" />
-          <Input label="Confirmar senha" type="password" />
+          <Input
+            label="Senha"
+            type="password"
+            required
+            value={senha}
+            onChange={(e: any) => {
+              handleChangeSenha(e.target.value, setErrorMessages, setSenha);
+              verificaSenhasIguais(e.target.value, confSenha, setErrorMessages);
+            }}
+          />
+          <Input
+            label="Confirmar senha"
+            type="password"
+            required
+            value={confSenha}
+            onChange={(e: any) => {
+              handleChangeConfSenha(
+                e.target.value,
+                setErrorMessages,
+                setConfSenha
+              );
+              verificaSenhasIguais(e.target.value, senha, setErrorMessages);
+            }}
+          />
         </div>
         <div className="form-actions flex-column-gap20">
           <Input type="reset" variant="bgNeutral" value="Limpar" />
