7b2e3f8f04ec6985094b16f13116259e08f8d9bd
GiseleBCSantos <catce.2024111tads0013@aluno.ifpi.edu.br>
2024-11-28 17:26:38 -0300

    #71 :sparkles: feat: Adiciona validação ao formulário de Permissoes

--- src/pages/permissions/create/index.tsx
+++ src/pages/permissions/create/index.tsx
@@ -1,17 +1,59 @@
-import { Link } from "react-router-dom";
+import { Link, useNavigate } from "react-router-dom";
 import { Button } from "../../../ui/button";
 import { Input } from "../../../ui/input";
 import "./styles.css";
+import { useState } from "react";
+import {
+  handleChangeDescription,
+  handleChangeRole,
+} from "../../../modules/permissionsFormValidation";
 
 const CreatePermission = () => {
+  const navigate = useNavigate();
+  const [role, setRole] = useState("");
+  const [descricao, setDescricao] = useState("");
+  const [errorMessages, setErrorMessages] = useState({});
+
+  const handleSubmit = (e: any) => {
+    e.preventDefault();
+
+    const listaErros = Object.values(errorMessages).filter(
+      (error) => error !== ""
+    );
+
+    if (listaErros.length > 0) {
+      console.log(listaErros[0]);
+    } else {
+      navigate("/permissoes");
+    }
+  };
+
   return (
     <div className="add-page flex-column-gap20">
       <h1>Cadastrar permissão</h1>
 
-      <form action="" className="form-registration flex-column-gap20">
+      <form
+        action=""
+        className="form-registration flex-column-gap20"
+        onSubmit={handleSubmit}
+      >
         <div className="form-inputs flex-column-gap20">
-          <Input label="Role" />
-          <Input label="Descrição" />
+          <Input
+            label="Role"
+            value={role}
+            required
+            onChange={(e: any) =>
+              handleChangeRole(e, setErrorMessages, setRole)
+            }
+          />
+          <Input
+            label="Descrição"
+            required
+            value={descricao}
+            onChange={(e: any) =>
+              handleChangeDescription(e, setErrorMessages, setDescricao)
+            }
+          />
         </div>
 
         <div className="form-actions-registration flex-column-gap20">
