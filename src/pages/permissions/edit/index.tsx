8ebef61ee76b48988c02352d5c7fa7129acab440
Thalisson Moura <thalissonmoura138@gmail.com>
2024-12-02 08:11:00 -0300

    :recycle:refactor: adiciona funcionalidade de edição a página de permissões

--- src/pages/permissions/edit/index.tsx
+++ src/pages/permissions/edit/index.tsx
@@ -1,24 +1,54 @@
-import { Link } from "react-router-dom";
+// EditPermission.tsx
+import { useLocation, useNavigate } from "react-router-dom";
 import { Button } from "../../../ui/button";
 import { Input } from "../../../ui/input";
 import "./styles.css";
+import { useState } from "react";
 
 const EditPermission = () => {
+  const { state } = useLocation();
+  const { permission } = state; 
+
+  const navigate = useNavigate();
+
+  const [role, setRole] = useState(permission.role);
+  const [description, setDescription] = useState(permission.description);
+
+  const handleSubmit = (e: any) => {
+    e.preventDefault();
+
+    if (!role || !description) {
+      alert("Por favor, preencha todos os campos.");
+      return;
+    }
+
+    const updatedPermission = { id: permission.id, role, description };
+
+    navigate("/permissoes", {
+      state: { updatedPermission },
+    });
+  };
+
   return (
     <div className="add-page flex-column-gap20">
       <h1>Editar permissão</h1>
 
-      <form action="" className="form-edit flex-column-gap20">
+      <form onSubmit={handleSubmit} className="form-edit flex-column-gap20">
         <div className="form-inputs flex-column-gap20">
-          <Input label="Role" />
-          <Input label="Descrição" />
+          <Input
+            label="Role"
+            value={role}
+            onChange={(e: any) => setRole(e.target.value)}
+          />
+          <Input
+            label="Descrição"
+            value={description}
+            onChange={(e: any) => setDescription(e.target.value)}
+          />
         </div>
 
         <div className="form-actions-edit flex-column-gap20">
-          <Input type="reset" variant="bgNeutral" value="Limpar" />
-          <Link to="/permissoes">
-            <Button label="Cancelar" />
-          </Link>
+          <Button label="Cancelar" onClick={() => navigate("/permissoes")} />
           <Input type="submit" variant="bgSuccess" value="Editar" />
         </div>
       </form>
