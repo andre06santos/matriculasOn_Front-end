22c190f9204c1d36cd385dd25cc14cf684f1ce30
Thalisson Moura <thalissonmoura138@gmail.com>
2024-12-08 13:39:47 -0300

    :recycle:refactor: altera a página de edição de permissões

--- src/pages/permissions/edit/index.tsx
+++ src/pages/permissions/edit/index.tsx
@@ -1,4 +1,4 @@
-import { Link } from "react-router-dom";
+import { Link, useLocation, useNavigate } from "react-router-dom";
 import { Button } from "../../../ui/button";
 import { Input } from "../../../ui/input";
 import "./styles.css";
@@ -9,18 +9,74 @@ import {
 } from "../../../modules/permissionsFormValidation";
 
 const EditPermission = () => {
+  const { state: permission } = useLocation();
+  const navigate = useNavigate();
+  const [role, setRole] = useState(permission.role);
+  const [descricao, setDescricao] = useState(permission.description);
+  const [errorMessages, setErrorMessages] = useState({});
+
+  console.log(permission);
+  const handleSubmit = (e: any) => {
+    e.preventDefault();
+
+    const listaErros = Object.values(errorMessages).filter(
+      (error) => error !== ""
+    );
+
+    if (listaErros.length > 0) {
+      console.log(listaErros[0]);
+    } else {
+      navigate("/permissoes");
+    }
+  };
+
+  const onClean = (e: any) => {
+    e.preventDefault();
+
+    setRole("");
+    setDescricao("");
+    setErrorMessages({});
+  };
+
   return (
     <div className="add-page flex-column-gap20">
       <h1>Editar permissão</h1>
 
-      <form action="" className="form-edit flex-column-gap20">
+      <form
+        action=""
+        className="form-edit flex-column-gap20"
+        onSubmit={handleSubmit}
+      >
         <div className="form-inputs flex-column-gap20">
-          <Input label="Role" />
-          <Input label="Descrição" />
+          <Input
+            label="Role"
+            value={role}
+            required
+            onChange={(e: any) =>
+              handleChangeRole(e.target.value, setErrorMessages, setRole)
+            }
+          />
+          <Input
+            label="Descrição"
+            required
+            value={descricao}
+            onChange={(e: any) =>
+              handleChangeDescription(
+                e.target.value,
+                setErrorMessages,
+                setDescricao
+              )
+            }
+          />
         </div>
 
         <div className="form-actions-edit flex-column-gap20">
-          <Input type="reset" variant="bgNeutral" value="Limpar" />
+          <Input
+            type="reset"
+            variant="bgNeutral"
+            value="Limpar"
+            onCLick={onClean}
+          />
           <Link to="/permissoes">
             <Button label="Cancelar" />
           </Link>
