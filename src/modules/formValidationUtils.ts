44bf813526fc80d44166a08da6ca264cd4eef9b7
GiseleBCSantos <catce.2024111tads0013@aluno.ifpi.edu.br>
2024-12-12 09:50:01 -0300

    :recycle: refactor: Altera regex de email

--- src/modules/formValidationUtils.ts
+++ src/modules/formValidationUtils.ts
@@ -13,7 +13,7 @@ export const CPF_LENGTH = 11;
 
 const regexOnlyLetters = new RegExp("^[a-zA-ZÀ-ÿ´`~^\\s]+$");
 const regexValidEmail = new RegExp(
-  "^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+.[a-zA-Z]{2,}$"
+  "^[a-zA-Z0-9._%+-]{1,}@[a-zA-Z0-9-]{1,}.[a-zA-Z]{2,}$"
 );
 const regexOnlyNumbers = new RegExp("[0-9]+$");
 const regexLettersAndNumbers = new RegExp("^[a-zA-Z0-9]+$");
