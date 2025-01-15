645d34d4588a13277f8c0b528ad4b55a9f46924a
Thalisson Moura <thalissonmoura138@gmail.com>
2025-01-15 15:30:41 -0300

    :recycle:refactor: adiciona lóggica de tipagem para permissões

--- src/modules/administradores/infrastructure/types.ts
+++ src/modules/administradores/infrastructure/types.ts
@@ -1,3 +1,5 @@
+import React from "react";
+
 /* Tipos de events */
 export type FormEventType = React.FormEvent<HTMLFormElement>;
 export type MouseEventType = React.MouseEvent<HTMLButtonElement>;
@@ -8,6 +10,12 @@ export type ChangeEventType = React.ChangeEvent<HTMLInputElement>;
 export type CursoType = {
   id?: string;
   nome: string;
+};
+
+export type PermissionsType = {
+  id?: string;
+  role: string;
+  descricao: string;
 };
 
 export type ErrorMessagesType = Record<string, string>[];
@@ -53,5 +61,13 @@ export type CursosFilterType = {
   name: string;
   setName: React.Dispatch<React.SetStateAction<string>>;
   nameInput: React.MutableRefObject<HTMLInputElement | null>;
+  onReset: () => void;
+};
+
+export type PermissionsFilterType = {
+  onSubmit: (e: FormEventType) => Promise<void>;
+  descricao: string;
+  setDescricao: React.Dispatch<React.SetStateAction<string>>;
+  descricaoInput: React.MutableRefObject<HTMLInputElement | null>;
   onReset: () => void;
 };
