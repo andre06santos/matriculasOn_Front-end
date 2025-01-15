e6ecdd123788b7b400a07aeb3efc919c0b4c9d20
Thalisson Moura <thalissonmoura138@gmail.com>
2025-01-15 11:13:40 -0300

    :recyle:refactor: implementa as classes e a constante visiblebuttons

--- src/ui/paginacao/index.tsx
+++ src/ui/paginacao/index.tsx
@@ -1,13 +1,13 @@
 import React from "react";
 import "./styles.css";
 
+const VISIBLEBUTTONS = 5;
 interface PaginationProps {
   currentPage: number;
   totalPages: number;
   onNext: () => void;
   onPrev: () => void;
   onPageChange: (page: number) => void;
-  visibleButtons: number;
 }
 
 const Pagination = ({
@@ -16,11 +16,10 @@ const Pagination = ({
   onNext,
   onPrev,
   onPageChange,
-  visibleButtons,
 }: PaginationProps) => {
   const buttons = [];
-  const start = Math.max(0, currentPage - Math.floor(visibleButtons / 2));
-  const end = Math.min(totalPages, start + visibleButtons);
+  const start = Math.max(0, currentPage - Math.floor(VISIBLEBUTTONS / 2));
+  const end = Math.min(totalPages, start + VISIBLEBUTTONS);
 
   for (let i = start; i < end; i++) {
     buttons.push(i);
@@ -28,23 +27,33 @@ const Pagination = ({
 
   return (
     <div className="pagination-container">
-      <button onClick={onPrev} disabled={currentPage === 0}>
+      <button
+        className="button-prev"
+        onClick={onPrev}
+        disabled={currentPage === 0}
+      >
         Voltar
       </button>
       {buttons.map((page) => (
         <button
           key={page}
           onClick={() => onPageChange(page)}
-          className={currentPage === page ? "active" : ""}
+          className={
+            currentPage === page ? "button-page active" : "button-page"
+          }
         >
           {page + 1}
         </button>
       ))}
-      <button onClick={onNext} disabled={currentPage === totalPages - 1}>
+      <button
+        className="button-next"
+        onClick={onNext}
+        disabled={currentPage === totalPages - 1}
+      >
         Avançar
       </button>
     </div>
   );
 };
 
-export default Pagination;
+export { Pagination };
