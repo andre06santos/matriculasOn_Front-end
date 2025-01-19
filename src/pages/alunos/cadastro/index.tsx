2ac19e99d363b64e6072f749be885f7b95508936
Thalisson <thalissonmoura138@gmail.com>
2025-01-19 17:02:15 -0300

    :recycle:refactor: implementa o tipo aluno

--- src/pages/alunos/cadastro/index.tsx
+++ src/pages/alunos/cadastro/index.tsx
@@ -25,6 +25,7 @@ import {
 } from "../../../modules/administradores/infrastructure/types";
 
 const RegisterStudent = () => {
+  const [tipo] = useState<string>("Aluno");
   const [cpf, setCpf] = useState<string>("");
   const [matricula, setMatricula] = useState<string>("");
   const [nome, setNome] = useState<string>("");
@@ -60,6 +61,7 @@ const RegisterStudent = () => {
         username,
         matricula,
         email,
+        tipo,
         curso: curso!.value,
       };
 
